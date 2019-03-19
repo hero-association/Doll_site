96a30003f7f6f601fb88471f21d91720d7917783
jasonpak17 <jason.baak.17@gmail.com>
2019-03-19 13:59:45 +0800

    Update models.py

--- doll_sites/models.py
+++ doll_sites/models.py
@@ -18,7 +18,6 @@ def banner_upload_location(instance,filename):
 class UserProfile(models.Model):
 	"""对Django自带的User表进行扩展"""
 	user = models.OneToOneField(User,on_delete=models.CASCADE, related_name='profile')
-	emails = models.EmailField(default=0,null=True,blank=True)
 	member_type = models.BooleanField(default=False)
 	member_expire = models.DateField(null=True,blank=True)		#会员过期时间
 	count_coin = models.DecimalField(default=0,max_digits=8,decimal_places=2) 	#金币余额
