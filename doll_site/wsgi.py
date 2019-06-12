19f467eb961ad53cfc5521fa1d233ff4aa00ab08
jasonpak17 <jason.baak.17@gmail.com>
2019-06-12 22:49:08 +0800

    Update wsgi.py

--- doll_site/wsgi.py
+++ doll_site/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import django.core.handlers.wsgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doll_site.settings')
 
 application = get_wsgi_application()
+
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+DOMAIN_NAME = 'www.lolizhan.net'
+def application(environ, start_response):
+  if environ['HTTP_HOST'] != DOMAIN_NAME:
+    location = DOMAIN_NAME + environ['PATH_INFO']
+    if environ.get('QUERY_STRING'):
+      location += '?' + environ['QUERY_STRING']
+    start_response('301 Redirect', [('Location', location),])
+    return []
+  return _application(environ, start_response)
