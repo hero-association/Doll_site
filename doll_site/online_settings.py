ff96a7acab4c8298d424e8c980f5eff616278c2f
charles026 <charles0260@outlook.com>
2019-08-05 18:02:45 +0800

    profile fix

--- doll_site/online_settings.py
+++ doll_site/online_settings.py
@@ -108,7 +108,8 @@ DEFAULT_FROM_EMAIL = 'service@alolita.net'
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-    '*'
+    'http://127.0.0.1:8000',
+    'http://localhost:8000'
 )
 
 CORS_ALLOW_METHODS = (
